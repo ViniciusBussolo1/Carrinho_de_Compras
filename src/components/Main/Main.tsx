4ddbd4719575629b2297e90f74c8e7ca35387b0f
ViniciusBussolo1 <vinicius.buss13@gmail.com>
2023-02-03 11:12:43 -0300

    add component table in main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,58 @@
+import { Products } from "../../data/produtos";
+
+import X from "../../assets/icon_X.svg";
+import Minus from "../../assets/icon_Minus.svg";
+import Plus from "../../assets/icon_Plus.svg";
+
 import "./styles.scss";
 
 export default function Main() {
-  return <h1>Mian</h1>;
+  return (
+    <main>
+      <h1>Seu Carrinho</h1>
+      <div className="container_main">
+        <table>
+          <thead>
+            <tr>
+              <th>Produto</th>
+              <th>Preço</th>
+              <th>Quantidade</th>
+              <th>Total</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {Products.map((product, index) => (
+              <tr key={index}>
+                <td>
+                  <div className="div_product">
+                    <img className="img" src={product.img} alt="" />
+                    <div>
+                      <span className="product_name">{product.name}</span>
+                      <span className="product_color">{product.color}</span>
+                    </div>
+                  </div>
+                </td>
+                <td className="td_price">R$ {product.price}</td>
+                <td className="product_quantity">
+                  <button className="button_fewer">-</button>
+                  <span>2</span>
+                  <button className="button_more">+</button>
+                </td>
+                <td className="td_total">R$ 240</td>
+                <td className="td_X">
+                  <button className="button_X">
+                    <img src={X} alt="" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <aside>
+          <h1>sadds</h1>
+        </aside>
+      </div>
+    </main>
+  );
 }
---
+++ src/data/produtos.ts
@@ -0,0 +1,20 @@
+export const Products = [
+  {
+    name: "Teclado RedDragon",
+    color: "Laranja, Cinza e preto",
+    img: "../src/assets/Img_Teclado.png",
+    price: "239,90",
+  },
+  {
+    name: "Mouse HyperX",
+    color: "Black",
+    img: "../src/assets/Img_Mouse.png",
+    price: "329,90",
+  },
+  {
+    name: "HeadSet RedDragon",
+    color: "Black",
+    img: "../src/assets/Img_HeadSet.png",
+    price: "239,90",
+  },
+];
